2f27067a81068a6c989cc35106edf38f5c16013a
Mayte Gimenez <la.reina.roja@gmail.com>
2011-08-18 01:44:02 +0200

    [iOS] Added Categories files

---
+++ iOS/Nellodee/Classes/Rol.m
@@ -0,0 +1,14 @@
+//
+//  Rol.m
+//  Nellodee
+//
+//  Created by Ada Hopper on 11/08/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "Rol.h"
+
+
+@implementation Rol
+
+@end
